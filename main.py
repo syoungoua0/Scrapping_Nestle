a8dcd38bccd8c84a04bf4b17f9f0419ba24557fa
Stephane YOUNGOUA <47081381+syoungoua0@users.noreply.github.com>
2025-04-08 21:33:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scraper import bing_search
+from downloader import download_pdf
 
 entreprise = "Nestlé"
 année = "2022"
@@ -7,7 +8,9 @@ site = "nestle.com"
 query = f"{entreprise} annual report {année} site:{site} filetype:pdf"
 resultats = bing_search(query)
 
-print("Résultats trouvés :")
-for url in resultats:
-    print(url)
+if resultats:
+    pdf_url = resultats[0]  # On prend le premier résultat
+    fichier_pdf = download_pdf(pdf_url)
+else:
+    print("Aucun lien PDF trouvé.")
 
