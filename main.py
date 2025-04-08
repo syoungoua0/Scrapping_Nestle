1975867a8e7bc8acecc2ccdbfe787e957a35d765
Stephane YOUNGOUA <47081381+syoungoua0@users.noreply.github.com>
2025-04-08 21:19:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from scraper import bing_search
+
+entreprise = "Nestlé"
+année = "2022"
+site = "nestle.com"
+
+query = f"{entreprise} annual report {année} site:{site} filetype:pdf"
+resultats = bing_search(query)
+
+print("Résultats trouvés :")
+for url in resultats:
+    print(url)
 
